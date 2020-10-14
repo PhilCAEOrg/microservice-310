d2df82824e1d01dafd0cfa2923ced938c8500327
PhilCAEUser <rbyETE4Ceu2iA4Z>
2020-10-14 05:21:28 +0000

    Init

---
+++ db/310_create_tables.sql
@@ -0,0 +1,16 @@
+--
+-- Database Schema:  caeschema
+-- Automatically generated sql script for the service 310, created by the CAE.
+-- --------------------------------------------------------
+
+--
+-- Table structure for table tab.
+--
+CREATE TABLE caeschema.tab (
+  test INTEGER ,
+CONSTRAINT test_PK PRIMARY KEY (test)
+ 
+);
+
+
+
